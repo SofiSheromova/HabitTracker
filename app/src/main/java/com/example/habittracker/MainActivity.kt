3f9bc140cd62ac7d7543baf8c8436321e4a17a8f
sofi.sher <sofi.sheromova@gmail.com>
2021-04-03 00:09:43 +0500

    fab for card creation

--- app/src/main/java/com/example/habittracker/MainActivity.kt
+++ app/src/main/java/com/example/habittracker/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
 import com.example.habittracker.cards.Card
 import com.example.habittracker.cards.CardsAdapter
 import com.example.habittracker.cards.Periodicity
@@ -58,11 +59,28 @@ class MainActivity : AppCompatActivity() {
 
         adapter = CardsAdapter(getCardClickListener(cardsRecycler), cards)
         cardsRecycler.adapter = adapter
+
+        setupCardCreateButton()
     }
 
     private fun getCardClickListener(cardsRecycler: RecyclerView): View.OnClickListener {
         return View.OnClickListener { view ->
             if (view == null) return@OnClickListener
+        }
+    }
+
+    private fun setupCardCreateButton() {
+        val icon = VectorDrawableCompat.create(
+            resources,
+            R.drawable.ic_baseline_add_24,
+            null
+        )
+        binding.cardCreateButton.setImageDrawable(icon)
+        binding.cardCreateButton.setOnClickListener {
+            if (it == null)
+                return@setOnClickListener
+//            val sendIntent = Intent(this, CardEditorActivity::class.java)
+//            startActivityForResult(sendIntent, Constants.CREATE_CARD)
         }
     }
 }
