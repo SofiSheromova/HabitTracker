3f4ffebc1b376ffcdac701ccf04cb81f949d12cb
sofi.sher <sofi.sheromova@gmail.com>
2021-04-05 17:12:15 +0500

    removed unused var

--- app/src/main/java/com/example/habittracker/Constants.kt
+++ app/src/main/java/com/example/habittracker/Constants.kt
@@ -4,12 +4,5 @@ object Constants {
     const val CREATE_CARD = 56
     const val EDIT_CARD = 45
     const val CARD_JSON = "CARD_JSON"
-    const val TITLE = "TITLE"
-    const val DESCRIPTION = "DESCRIPTION"
-    const val PRIORITY = "PRIORITY"
-    const val TYPE = "TYPE"
-    const val COLOR = "COLOR"
-    const val REPETITIONS_NUMBER = "REPETITIONS_NUMBER"
-    const val DAYS_NUMBER = "DAYS_NUMBER"
     const val CARD_POSITION = "POSITION"
 }
