112667853b14d59467ccb80e9df8a5814fafa938
sofi.sher <sofi.sheromova@gmail.com>
2021-04-08 17:29:00 +0500

    closing the keyboard when switching fragments

--- app/src/main/java/com/example/habittracker/MainActivity.kt
+++ app/src/main/java/com/example/habittracker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.habittracker
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,8 +13,10 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
@@ -24,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
+        navController.addOnDestinationChangedListener(this)
         appBarConfiguration = AppBarConfiguration(
             setOf(R.id.nav_home, R.id.nav_info), drawerLayout
         )
@@ -31,8 +37,27 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        val navController = findNavController(R.id.nav_host_fragment)
+        navController.removeOnDestinationChangedListener(this)
+    }
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
+        currentFocus?.hideKeyboard()
+    }
+
+    fun View.hideKeyboard() {
+        val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
     }
 }
