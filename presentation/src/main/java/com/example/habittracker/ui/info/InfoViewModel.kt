4f05b4a164dfb986eb5c68b506262d814903fa66
sofi.sher <sofi.sheromova@gmail.com>
2021-06-15 14:05:18 +0500

    info page

--- presentation/src/main/java/com/example/habittracker/ui/info/InfoViewModel.kt
+++ presentation/src/main/java/com/example/habittracker/ui/info/InfoViewModel.kt
@@ -1,13 +1,7 @@
 package com.example.habittracker.ui.info
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class InfoViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is info Fragment"
-    }
-    val text: LiveData<String> = _text
+    val version: String = "1.0.0"
 }
